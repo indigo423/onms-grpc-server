801f423330a5ba1b72e521aa82311fe5f08b6945
Alejandro Galue <agalue@opennms.org>
2021-03-28 10:24:19 -0400

    Fix protobuf code generation.

--- protobuf/build.sh
+++ protobuf/build.sh
@@ -2,6 +2,6 @@
 
 type protoc >/dev/null 2>&1 || { echo >&2 "protoc required but it's not installed; aborting."; exit 1; }
 
-protoc -I . ./proto/sink-message.proto --go_out=./sink
-protoc -I . ./proto/kafka-rpc.proto --go_out=./rpc
-protoc -I . ./proto/ipc.proto --go_out=plugins=grpc:./ipc
+protoc --proto_path=./proto --go_out=./ sink-message.proto
+protoc --proto_path=./proto --go_out=./ kafka-rpc.proto
+protoc --proto_path=./proto --go_out=plugins=grpc:./ ipc.proto
